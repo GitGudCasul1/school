b276789c63d16868fba4dfba438aeab46f97a98c
Jef Den Hond <jdenhond@ION.local>
2019-11-26 10:44:38 +0100

    niewe oefeningen

---
+++ Toetsen/ToetsVersiebeheer/ArrayPrinter.java
@@ -0,0 +1,10 @@
+public class ArrayPrinter{
+	public static void main(String[] args) {
+		ArrayPrinter praktijk = new ArrayPrinter();
+      		int[] array = new int[]{1, 2, 3, 4, 5};		
+	}
+	private void printarray(){}
+}
+
+
+
